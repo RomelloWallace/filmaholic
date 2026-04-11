33ed92196723789895c2092996d090820d6dd506
RomelloWallace <romellowallace01@gmail.com>
2026-04-11 14:53:46 -0600

    shell of app

---
+++ Filmaholic.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Filmaholic.App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "Filmaholic.App" };
+	}
+}
---
+++ Filmaholic.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Filmaholic.App;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
