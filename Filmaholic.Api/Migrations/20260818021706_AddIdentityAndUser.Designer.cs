99d80b01a004ac797a14922fe71cfef84da8bbc5
RomelloWallace <romellowallace01@gmail.com>
2026-08-17 20:21:55 -0600

    Update db

--- Filmaholic.Api/Migrations/20260818021706_AddIdentityAndUser.Designer.cs
+++ Filmaholic.Api/Migrations/20260818021706_AddIdentityAndUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Filmaholic.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Filmaholic.Api.Migrations
 {
     [DbContext(typeof(MoviesDbContext))]
-    partial class MoviesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260818021706_AddIdentityAndUser")]
+    partial class AddIdentityAndUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
